5b498e8936f1c8e27c2faf376dab51e481d5d291
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-07-12 20:55:16 +0600

    initialization method call added.

--- src/main/java/com/morshed/netmeter/NetMeterController.java
+++ src/main/java/com/morshed/netmeter/NetMeterController.java
@@ -3,6 +3,7 @@ package com.morshed.netmeter;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -10,8 +11,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.util.Duration;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class NetMeterController {
+
+public class NetMeterController implements Initializable {
     @FXML
     public Label welcomeText;
     @FXML
@@ -29,7 +33,7 @@ public class NetMeterController {
     public Image inboundImage;
     public Image outboundImage;
 
-    public NetMeterController() {
+    public NetMeterController() throws InterruptedException {
         inboundImage = new Image("/down.png");
         outboundImage = new Image("/up.png");
         inboundImageView = new ImageView();
@@ -70,5 +74,10 @@ public class NetMeterController {
     @FXML
     public void close(){
         Platform.exit();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hideButton();
     }
 }
